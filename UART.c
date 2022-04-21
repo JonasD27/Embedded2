7c21d908566d19f383b321b3ed277b01f58e5fa5
Marci-HS <102648274+Marci-HS@users.noreply.github.com>
2022-04-21 13:57:32 +0200

    Add files via upload

---
+++ UART.c
@@ -0,0 +1,127 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+#include "UART.h"
+#include "system.h"
+
+/******************************************************************************/
+/* Funktionen                                                                 */
+/******************************************************************************/
+
+void initUART()
+{
+    U1MODEbits.STSEL = 0; // 1-Stop bit
+    U1MODEbits.PDSEL = 0; // No Parity, 8-Data bits
+    U1MODEbits.ABAUD = 0; // Auto-Baud disabled
+    U1MODEbits.UEN = 0;
+    U1MODEbits.LPBACK = 0;
+    U1MODEbits.RXINV = 0;
+    //U1MODEbits.ALTIO = 0;
+    
+    U1MODEbits.URXINV = 0;
+    U1MODEbits.RTSMD = 0;
+    
+    U1MODEbits.BRGH = 0; // Standard-Speed mode
+    U1BRG = BRGVAL; // Baud Rate setting for 9600
+    
+    U1STAbits.UTXISEL0 = 0; // Interrupt after one TX character is transmitted
+    U1STAbits.UTXISEL1 = 0;
+    U1STAbits.UTXBRK = 0;
+    U1STAbits.ADDEN = 0;
+    U1STAbits.UTXINV = 0;
+    U1STAbits.URXISEL = 0;
+    U1STA = U1STA | 0b0001000000000000;
+    //_URXEN = 1;
+    
+    //_U1RXIE = 1; // Enable UART RX interrupt
+    
+    U1MODEbits.UARTEN = 1; // Enable UART
+    delay_ms(2);
+    U1STAbits.UTXEN = 1; // Enable UART TX
+    
+    /* Wait at least 105 microseconds (1/9600) before sending first char */
+    delay_ms(2); 
+    _U1TXIE = 1; // Enable UART TX interrupt
+    
+} /* initUART() */
+
+
+void __attribute__((__interrupt__, no_auto_psv)) _U1TXInterrupt(void)
+{ 
+    _U1TXIF = 0; // Clear TX Interrupt flag
+    getcFIFO_TX(&U1TXREG);
+    
+}
+
+int16_t putcFIFO_TX(char c)
+{
+  //if (buffer.write >= BUFFER_SIZE)
+  //  buffer.write = 0; // erhöht sicherheit
+  _LATF0 = 1;
+  if ( ( FIFO.write + 1 == FIFO.read ) ||
+       ( FIFO.read == 0 && FIFO.write + 1 == BUFFER_SIZE ) )
+  {
+    return BUFFER_FAIL; // voll
+  }
+
+  FIFO.data[FIFO.write] = c;
+
+  FIFO.write++;
+  if (FIFO.write >= BUFFER_SIZE)
+  {
+    FIFO.write = 0;
+  }
+  return BUFFER_SUCCESS;
+  
+} /* putcFIFO_TX() */
+
+int16_t getcFIFO_TX(volatile uint16_t *c)
+{
+  _LATF0 = 1;
+  if (FIFO.read == FIFO.write)
+  {
+    return BUFFER_FAIL;
+  }
+  *c = FIFO.data[FIFO.read];
+
+  FIFO.read++;
+  if (FIFO.read >= BUFFER_SIZE)
+  {
+    FIFO.read = 0;
+  }
+  return BUFFER_SUCCESS;
+  
+} /* getcFIFO_TX() */
+
+int16_t putcUART(char c)
+{
+    _LATF0 = 1;
+    _GIE = 0; // Interrupts ausschalten
+    int16_t erfolg = putcFIFO_TX(c);
+    _GIE = 1;
+    return erfolg;
+    
+} /* putcUART() */
+
+int16_t putsUART(const char *str) 
+{
+    _LATF0 = 1;
+    uint16_t i;
+    uint16_t length = strlen(str);
+    
+    _GIE = 0;   //Global Interrupt disable
+    for(i = 0; i < length; i++) 
+    {
+        //uint16_t ret = putcFIFO_TX(str[i]);
+        if(! putcFIFO_TX(str[i]))
+            break;
+    }
+    _GIE = 1;
+    int16_t erfolg = -i;
+    if(erfolg == -length)
+        erfolg *= -1;
+    _U1TXIF = 1; //Interuppt Routine Starten um FIFO-Inhalt zu senden
+    return erfolg;
+    
+} /* putsUART() */
