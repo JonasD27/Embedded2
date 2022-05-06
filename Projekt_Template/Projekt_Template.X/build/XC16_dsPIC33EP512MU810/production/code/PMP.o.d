568ffad9a618b395a55b6abe8202fdc73e50c109
Marci-HS <102648274+Marci-HS@users.noreply.github.com>
2022-05-06 12:10:10 +0200

    update

--- Projekt_Template/Projekt_Template.X/build/XC16_dsPIC33EP512MU810/production/code/PMP.o.d
+++ Projekt_Template/Projekt_Template.X/build/XC16_dsPIC33EP512MU810/production/code/PMP.o.d
@@ -1,1 +1,4 @@
-build/XC16_dsPIC33EP512MU810/production/code/PMP.o: code/PMP.c
+build/XC16_dsPIC33EP512MU810/production/code/PMP.o:  \
+code/PMP.c  \
+ code/system.h  \
+code/user.h 
--- Projekt_Template/Projekt_Template.X/code/PMP.c
+++ Projekt_Template/Projekt_Template.X/code/PMP.c
@@ -1,1 +1,25 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
 
+#include <xc.h>            /* Device header file                              */
+
+#include <stdio.h>        
+#include <stdint.h>        /* Includes uint16_t definition                    */
+#include <stdbool.h>       /* Includes true/false definition                  */
+#include <stdlib.h>
+#include <string.h>
+
+#include "system.h"        /* System funct/params, like osc/peripheral config */
+#include "user.h"          /* User funct/params, such as InitApp              */
+
+
+/******************************************************************************/
+/* Functions                                                                  */
+/******************************************************************************/
+
+void initPMP(void)
+{
+    _PMPEN = _PMPEN | 1;
+    
+}/*initPMP()*/
