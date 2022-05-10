2c211c1a89dddac34b51d846d57cb23e66cadf66
Marci-HS <102648274+Marci-HS@users.noreply.github.com>
2022-05-10 10:19:48 +0200

    update

--- Projekt_Template/Projekt_Template.X/build/XC16_dsPIC33EP512MU810/debug/code/PMP.o.d
+++
@@ -1,8 +0,0 @@
-build/XC16_dsPIC33EP512MU810/debug/code/PMP.o: code/PMP.c code/PMP.h \
- code/user.h code/system.h
-
-code/PMP.h:
-
-code/user.h:
-
-code/system.h:
--- Projekt_Template/Projekt_Template.X/build/XC16_dsPIC33EP512MU810/debug/code/interrupts.o.d
+++
@@ -1,6 +0,0 @@
-build/XC16_dsPIC33EP512MU810/debug/code/interrupts.o: code/interrupts.c \
- code/user.h code/I2C.h
-
-code/user.h:
-
-code/I2C.h:
