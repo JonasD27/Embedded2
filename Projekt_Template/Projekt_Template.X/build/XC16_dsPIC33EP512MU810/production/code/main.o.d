dbeec7c167346e33e3b1d2b3ad6222b3533e863d
JonasD27 <jonas.doll@gmx.de>
2022-05-13 09:59:36 +0200

    update

--- Projekt_Template/Projekt_Template.X/build/XC16_dsPIC33EP512MU810/production/code/main.o.d
+++ Projekt_Template/Projekt_Template.X/build/XC16_dsPIC33EP512MU810/production/code/main.o.d
@@ -1,6 +1,5 @@
 build/XC16_dsPIC33EP512MU810/production/code/main.o: code/main.c \
- code/system.h code/user.h code/UART.h code/I2C.h code/PMP.h \
- code/gesture9.h
+ code/system.h code/user.h code/UART.h code/I2C.h code/PMP.h
 
 code/system.h:
 
@@ -11,5 +10,3 @@ code/UART.h:
 code/I2C.h:
 
 code/PMP.h:
-
-code/gesture9.h:
